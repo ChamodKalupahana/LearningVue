7fb7b5548d1686253088f252eccbaad85174f41b
ChamodKalupahana <Chamodkalupahana@gmail.com>
2025-04-23 21:35:53 +0100

    got our app working from assignemtns.js

--- 7.Bringing It All Together/js/components/App.js
+++ 7.Bringing It All Together/js/components/App.js
@@ -1,29 +1,10 @@
-import AppButton from "./AppButton.js"
+import Assignments from "./Assignments.js"
 
 export default {
+  components : {Assignments}
+  , 
     template : `
-
+      <Assignments></Assignments>
     `,
-    data() {
-      return {
-        assignments : [
-          {name : 'Finish project', complete : false, id: 1},
-          {name : 'Read chapter 4', complete : false, id: 2},
-          {name : 'turn in homework', complete : false, id : 3},
-        ]
-      }
-    },
-    methods : {
-      toggle() {
-        this.complete = ! this.complete
-      }
-    },
-    computed : {
-      completedAssignments() {
-        return this.assignments.filter(assignment => assignment.complete);
-      },
-      InProgressAssignments() {
-        return this.assignments.filter(assignment => ! assignment.complete);
-      }
-    }
+
   }
---
+++ 7.Bringing It All Together/js/components/Assignments.js
@@ -0,0 +1,64 @@
+export default {
+    template : `
+          <section v-show="InProgressAssignments.length">
+    <h2 class="font-bold mb-2"> In Progress Assignments </h2>
+
+    <ul>
+      <li 
+      v-for="assignment in InProgressAssignments" 
+      :key="assignment.id"> 
+        <label>
+          {{ assignment.name}}
+          
+          <input type="checkbox" v-model="assignment.complete">
+        </label>
+      </li>
+    </ul>
+
+    <!-- <pre>
+      {{assignments}}
+
+    </pre> -->
+  </section>
+
+  <section v-show="completedAssignments.length" class="mt-10">
+    <h2 class="font-bold mb-2"> Completed Assignments </h2>
+
+    <ul>
+      <li 
+      v-for="assignment in completedAssignments" 
+      :key="assignment.id"> 
+        <label>
+          {{ assignment.name}}
+          
+          <input type="checkbox" v-model="assignment.complete">
+        </label>
+      </li>
+    </ul>
+
+
+  </section>
+    `,
+    data() {
+        return {
+          assignments : [
+            {name : 'Finish project', complete : false, id: 1},
+            {name : 'Read chapter 4', complete : false, id: 2},
+            {name : 'turn in homework', complete : false, id : 3},
+          ]
+        }
+      },
+      methods : {
+        toggle() {
+          this.complete = ! this.complete
+        }
+      },
+      computed : {
+        completedAssignments() {
+          return this.assignments.filter(assignment => assignment.complete);
+        },
+        InProgressAssignments() {
+          return this.assignments.filter(assignment => ! assignment.complete);
+        }
+      }
+}
