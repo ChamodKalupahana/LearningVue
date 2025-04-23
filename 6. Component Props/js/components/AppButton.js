f3397051bcfba3e483c979e14a0de8656bea6d03
ChamodKalupahana <Chamodkalupahana@gmail.com>
2025-04-23 01:30:06 +0100

    got the stlying to change

--- 6. Component Props/js/components/AppButton.js
+++ 6. Component Props/js/components/AppButton.js
@@ -1,6 +1,12 @@
 export default {
     template: `
-      <button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing"> 
+      <button 
+      :class="{
+        'border rounded px-5 py-2 disabled:cursor-not-allowed': true,
+        'bg-gray-200 hover:bg-gray-400'
+      }"
+      :disabled="processing"
+      > 
         <slot></slot>
       </button>
     `,
