ef3ae85f604e2135307179d8a9450c85096b8d37
ChamodKalupahana <Chamodkalupahana@gmail.com>
2025-04-23 01:32:44 +0100

    got the cases to work

--- 6. Component Props/js/components/AppButton.js
+++ 6. Component Props/js/components/AppButton.js
@@ -3,7 +3,9 @@ export default {
       <button 
       :class="{
         'border rounded px-5 py-2 disabled:cursor-not-allowed': true,
-        'bg-gray-200 hover:bg-gray-400'
+        'bg-blue-200 hover:bg-gray-400' : type === 'primary',
+        'bg-purple-200 hover:bg-gray-400' : type === 'secondary',
+        'bg-gray-200 hover:bg-gray-400' : type === 'muted'
       }"
       :disabled="processing"
       > 
