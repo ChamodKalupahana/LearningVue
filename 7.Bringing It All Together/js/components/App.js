288f48cfac501098335af63d186d5dc38578d895
ChamodKalupahana <Chamodkalupahana@gmail.com>
2025-04-23 21:25:23 +0100

    moved app data properties into appjs

--- 7.Bringing It All Together/js/components/App.js
+++ 7.Bringing It All Together/js/components/App.js
@@ -40,5 +40,27 @@ export default {
 
 
   </section>
-    `
+    `,
+    data() {
+      return {
+        assignments : [
+          {name : 'Finish project', complete : false, id: 1},
+          {name : 'Read chapter 4', complete : false, id: 2},
+          {name : 'turn in homework', complete : false, id : 3},
+        ]
+      }
+    },
+    methods : {
+      toggle() {
+        this.complete = ! this.complete
+      }
+    },
+    computed : {
+      completedAssignments() {
+        return this.assignments.filter(assignment => assignment.complete);
+      },
+      InProgressAssignments() {
+        return this.assignments.filter(assignment => ! assignment.complete);
+      }
+    }
   }
