cc04347f7910aeb8428b1d0fc6b7301b1ed1bda5
ChamodKalupahana <Chamodkalupahana@gmail.com>
2025-04-23 01:12:49 +0100

    moved app to its own file

---
+++ 6. Component Props/js/components/App.js
@@ -0,0 +1,11 @@
+import AppButton from "./js/components/AppButton.js"
+
+export default {
+    data() {
+      return {
+        message : "hello worl  dssd"
+      }
+    }, components : {
+      'app-button' : AppButton
+    }
+  }
